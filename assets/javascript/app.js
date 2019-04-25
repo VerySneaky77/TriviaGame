00aa7d94e189944439474a8f2a9cef319abe7b61
Carlos Quinto <carlquin77@gmail.com>
2019-04-25 14:44:24 -0700

    html elements in place, question structure added for script

---
+++ assets/javascript/app.js
@@ -0,0 +1,33 @@
+const timerQuestionMax = 20000;
+const timerNewQuestion = 4000;
+
+$(document).ready(function() {
+    var questionsTree = {
+        q1: {
+            question: "?",
+            choices: ["a", "b", "c", "d"],
+            answerIndex: 1,
+        },
+
+        q2: {
+            question: "?",
+            choices: ["a", "b", "c", "d"],
+            answerIndex: 0,
+        },
+
+        q3: {
+            question: "?",
+            choices: ["a", "b", "c", "d"],
+            answerIndex: 3,
+        }
+    }
+
+    $(".choice-display").on("click", function() {
+        
+    });
+
+    function popNewQuestion(previous) {
+        
+        
+    }
+});
