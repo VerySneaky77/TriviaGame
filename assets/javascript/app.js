93423c1948574212faefc49722f9039bcf665101
Carlos Quinto <carlquin77@gmail.com>
2019-04-26 02:21:37 -0700

    added timing functionality to script

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,19 +1,25 @@
 // Timer specs
 const timerQuestionMax = 10000;
 const timerNewQuestion = 4000;
-// Answer html arrays
-const $choiceRadios = [$("#answer-a"), $("#answer-b"), $("#answer-c"), $("#answer-d")];
-const $choiceLabels = [$("#answer-a-label"), $("#answer-b-label"), $("#answer-c-label"), $("#answer-d-label")];
 
 // Progress tracking
 var currentProgress = 0;
+var previousProgress = 0;
 var currentCorrect = 0;
 var currectIncorrect = 0;
 
 $(document).ready(function () {
+    // Answer html arrays
+    const $choiceRadios = [$("#answer-a"), $("#answer-b"), $("#answer-c"), $("#answer-d")];
+    const $choiceLabels = [$("#answer-a-label"), $("#answer-b-label"), $("#answer-c-label"), $("#answer-d-label")];
+
     // Current answer
     var currentAnswerIndex;
     var currentAnswerString;
+    // Timers
+    var questionTimer;
+    var timerTicker;
+    var nextTickTime = timerQuestionMax;
 
     var questionsTree = [
         {
@@ -44,51 +50,100 @@ $(document).ready(function () {
     $("button").on("click", function () {
         $("#button-container").addClass("no-show");
         $("#quiz-container").removeClass("no-show");
+        popQuiz();
+        // Start timers
+        rigQuestionTimeout();
+        rigTimerTicker();
     });
 
-    function quizTime(previous) {
-        setTimeout();
-
+    // Question limit timer
+    function rigQuestionTimeout() {
+        questionTimer = setTimeout(function () {
+            timeUp(2);
+        }, timerNewQuestion);
     }
 
-    $("input[name=answers]").on("change", function (e) {
-            var radioAnswer = parseInt($(this).val());
+    // New question timer
+    function rigNewQuestionTimer() {
+        questionTimer = setTimeout(function () {
+            rigQuestionTimeout();
+            rigTimerTicker();
+        }, timerNewQuestion);
+    }
 
-            // Correct selection
-            if (radioAnswer === currentAnswerIndex) {
-                $("#choice-display").text("Correct!");
-            }
-            // Incorrect selection
-            else {
-                $("#choice-display").text("Oops...");
-            }
-            $("input[name=answers]").attr("disabled", true);
-            $("#answer-row").removeClass("hide");
+    // Time remaining
+    function rigTimerTicker() {
+        timerTicker = setInterval(function () {
+            nextTickTime--;
+            $("#timer").text(nextTickTime);
+        }, 1000);
+    }
+
+    // Listen for input
+    $("input[name=answers]").on("change", function (e) {
+        // Clear timers
+        clearTimeout(questionTimer);
+        clearTimeout(rigTimerTicker);
+        // Check answer
+        timeUp(parseInt($(this).val()));
     })
+
+    function timeUp(choice) {
+        // Correct selection
+        if (choice === currentAnswerIndex) {
+            $("#choice-display").text("Correct!");
+            currentCorrect++;
+        }
+        // Timeout
+        else if (choice === -1) {
+            $("#choice-display").text("Out of time.");
+            currectIncorrect++;
+        }
+        // Incorrect selection
+        else {
+            $("#choice-display").text("Oops...");
+            currectIncorrect++;
+        }
+
+        updateQAUI(false, true);
+
+        currentProgress++;
+        // Prep time before a new question pops
+        rigNewQuestionTimer();
+    }
 
     function popQuiz() {
         // Check progress
         if (currentProgress < questionsTree.length) {
             currentAnswerIndex = questionsTree[currentProgress].answerIndex;
             currentAnswerString = questionsTree[currentProgress].choices[currentAnswerIndex];
-            console.log(currentAnswerIndex);
 
-            $("#answer-row").addClass("hide");
+            // Update question and answer html
+            updateQAUI(true, false);
+        }
+    }
 
+    function updateQAUI(hideAnswer, disableRadios) {
+        // Check if question needs reset
+        if (previousProgress !== currentProgress) {
             $("#question-display").text(questionsTree[currentProgress].question);
+
             // Populate selectable choices
             for (let i = 0; i < $choiceRadios.length; i++) {
                 $choiceRadios[i].val(i);
                 $choiceLabels[i].text(questionsTree[currentProgress].choices[i]);
             }
-            // Populate correct answer choice
-            $("#answer-display").text("The correct answer is: " + currentAnswerString);
-        }
-        // The quiz is done
-        else {
 
+            previousProgress = currentProgress;
         }
+
+        // Hide or reveal answer
+        if (hideAnswer) { $("#answer-row").removeClass("hide"); }
+        else { $("#answer-row").addClass("hide"); }
+
+        // Populate correct answer choice
+        $("#answer-display").text("The correct answer is: " + currentAnswerString);
+        // Enable or disable radio buttons
+        $("input[name=answers]").attr("disabled", disableRadios);
     }
-
-    popQuiz();
 });
